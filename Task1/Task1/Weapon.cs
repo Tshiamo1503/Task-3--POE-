408fd18661f88897b79a1ba7c1140da7a2da418d
Tshiamo1503 <69838544+Tshiamo1503@users.noreply.github.com>
2020-11-27 21:40:50 +0200

    adding Weapon class

---
+++ Task1/Task1/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    abstract class Weapon : Item
+    {
+        protected int Damage;
+        public int damage { get { return Damage; } set { Damage = value; } }
+
+        protected int Range;
+        public virtual int range { get { return Range; } set { Range = value; } }
+
+        protected int Durability;
+        public int durability { get { return Durability; } set { Durability = value; } }
+
+        protected int Cost;
+        public int cost { get { return Cost; } set { Cost = value; } }
+
+        protected string WeaponType;
+        public string weapontype { get { return WeaponType; } set { WeaponType = value; } }
+
+        public Weapon(char Symbl, int x, int y)
+            :base(x,y)
+        {
+
+        }
+    }
+}
