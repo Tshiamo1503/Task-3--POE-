25e79b24f67173f37dd73129d7b864204b1d5d5f
Tshiamo1503 <69838544+Tshiamo1503@users.noreply.github.com>
2020-11-27 23:08:07 +0200

    Creating Leader class

---
+++ Task1/Task1/Leader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Leader: Enemy
+    {
+        private Tile target;
+        public Tile TARGET { get { return target; } set { target = value; } }
+
+        public Leader(int x,int y) :base(x,y,2,20,'L')
+        {
+
+        }
+
+        public override Movement ReturnMove(Movement move = Movement.Nothing)//////////////////============ complete
+        {
+            int Direction;
+            Random ran = new Random();
+
+
+
+            return base.ReturnMove(move);
+        }
+    }
+}
